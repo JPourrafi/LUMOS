1bb16b525cd31326768e2b98d37c35b673ac73d1
JPourrafi <135212858+JPourrafi@users.noreply.github.com>
2024-07-01 21:26:58 +0330

    multiplier

--- Fixed_Point_Unit.v
+++ Fixed_Point_Unit.v
@@ -65,7 +65,52 @@ module Fixed_Point_Unit
     reg     [31 : 0] partialProduct2;
     reg     [31 : 0] partialProduct3;
     reg     [31 : 0] partialProduct4;
+    
+    reg[2:0] mul_state;
 
+    always @(posedge reset)
+    begin
+        if (reset) begin
+            mul_state <= 3'd0;
+            product_ready <= 1'b0;
+        end else if (operation == `FPU_MUL) begin
+            case (mul_state)
+                3'd0: begin // Start LL multiplication
+                    multiplierCircuitInput1 <= operand_1[15:0];
+                    multiplierCircuitInput2 <= operand_2[15:0];
+                    mul_state <= 3'd1;
+                end
+                3'd1: begin // LL multiplication done, start LH
+                    partialProduct1 <= multiplierCircuitResult;
+                    multiplierCircuitInput1 <= operand_1[15:0];
+                    multiplierCircuitInput2 <= operand_2[31:16];
+                    mul_state <= 3'd2;
+                end
+                3'd2: begin // LH multiplication done, start HL
+                    partialProduct2 <= multiplierCircuitResult;
+                    multiplierCircuitInput1 <= operand_1[31:16];
+                    multiplierCircuitInput2 <= operand_2[15:0];
+                    mul_state <= 3'd3;
+                end
+                3'd3: begin // HL multiplication done, start HH
+                    partialProduct3 <= multiplierCircuitResult;
+                    multiplierCircuitInput1 <= operand_1[31:16];
+                    multiplierCircuitInput2 <= operand_2[31:16];
+                    mul_state <= 3'd4;
+                end
+                3'd4: begin // HH multiplication done, combine results
+                    partialProduct4 <= multiplierCircuitResult;
+                    product <= {partialProduct4, 32'b0} + {partialProduct3, 16'b0} + {partialProduct2, 16'b0} + partialProduct1;
+                    product_ready <= 1'b1;
+                    mul_state <= 3'd0;
+                end
+                default: mul_state <= 3'd0;
+            endcase
+        end else begin
+            mul_state <= 3'd0;
+            product_ready <= 1'b0;
+        end
+    end
         /*
          *  Describe Your 32-bit Multiplier Circuit Here.
          */
